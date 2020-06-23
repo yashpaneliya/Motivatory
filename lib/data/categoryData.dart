0d6b60ec4edd3e34dc6c79f77a3fe42721e28e7b
Yash Paneliya <yashsp1158@gmail.com>
2020-06-23 15:52:17 +0530

    quotes data added

---
+++ lib/data/categoryData.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+List<Map<String,dynamic>> categories = [
+  {
+    'icon':Icons.insert_emoticon,
+    'title':"Happy"
+  },
+  {
+    'icon':Icons.thumb_up,
+    'title':"Motivation"
+  },
+  {
+    'icon':MdiIcons.crown,
+    'title':"Attitude"
+  },
+  {
+    'icon':MdiIcons.arrowTopRightThick,
+    'title':"Ambition"
+  },
+  {
+    'icon':Icons.sentiment_dissatisfied,
+    'title':"Lost"
+  },
+];
---
+++ lib/data/quotesData.dart
@@ -0,0 +1,36 @@
+List<Map<String, dynamic>> quotes = [
+  {
+    'author': 'Anonymous',
+    'quote':
+        'All our dreams can come true, if we have the courage to pursue them',
+    'liked': false,
+    'category': 'Motivation'
+  },
+  {
+    'author': 'Yash',
+    'quote': 'If you think you are bad then I am your dad',
+    'liked': false,
+    'category': 'Attitude'
+  },
+  {
+    'author': 'Shakti Boss',
+    'quote':
+        'play store ma aapdi 500 apps hovi joyi',
+    'liked': false,
+    'category': 'Ambition'
+  },
+  {
+    'author': 'TirthRaj',
+    'quote':
+        'Apna kam banta bhaad me jaye janta',
+    'liked': false,
+    'category': 'Attitude'
+  },
+  {
+    'author': 'Rahul Python God',
+    'quote':
+        'Koi mujhe mera kam bata do',
+    'liked': false,
+    'category': 'Lost'
+  },
+];
