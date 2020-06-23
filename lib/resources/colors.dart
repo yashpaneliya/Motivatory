b6d19f8c073c1ae8ba2648d980c973c7ddebcc77
Yash Paneliya <yashsp1158@gmail.com>
2020-06-23 15:51:28 +0530

    Colors and textstyles added

---
+++ lib/resources/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+//primary colors
+Color red=Colors.red;
+Color background=Color.fromRGBO(18,18,18,1);
+Color catcolor=Color.fromRGBO(39,39,39,1);
+
+//colors for category tabs
+List<Color> catTabBackColors=[
+  Colors.red,
+  Colors.yellow,
+  Colors.orange,
+  Colors.cyan,
+  Color.fromRGBO(0, 252, 183, 1),
+  Color.fromRGBO(252, 0, 227, 1),
+  Colors.green
+];
---
+++ lib/resources/styles.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+TextStyle quoteStyle=TextStyle(
+  fontSize: 35.0,
+  color: Colors.white,
+  letterSpacing: 1.0,
+  fontWeight: FontWeight.bold
+);
+
+TextStyle authorStyle=TextStyle(
+  fontSize: 25.0,
+  color: Colors.white,
+  letterSpacing: 1.0,
+  fontWeight: FontWeight.bold
+);
+
+TextStyle menuStyle=TextStyle(
+  fontWeight: FontWeight.bold,
+  letterSpacing: 1.0
+);
+
+TextStyle catTitleStyle=TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 20.0,
+  letterSpacing: 1.0
+);
