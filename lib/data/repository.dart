39c9500c98eac39175358e6ca0a24c3ed3895b39
Yash Paneliya <yashsp1158@gmail.com>
2020-07-04 15:35:10 +0530

    unnecessary code removed

--- lib/data/repository.dart
+++ lib/data/repository.dart
@@ -1,64 +1,66 @@
-import 'package:motivatory/data/database_creator.dart';
-import 'package:motivatory/data/quotesData.dart';
+// import 'package:motivatory/data/database_creator.dart';
+// import 'package:motivatory/data/quotesData.dart';
 
-class Repository{
 
-  //function to get all quotes
-  static Future<List<Quote>> getAllQuotes()async{
-    final sql='''SELECT * FROM ${Quotes.quoteTable}''';
-    final data=await db.rawQuery(sql);
-    List<Quote> quotesList=List();
 
-    for(final node in data){
-      var temp=Quote.fromJson(node);
-      quotesList.add(temp);
-    }
+//   //function to get all quotes
+//   Future<List<Quote>> getAllQuotes()async{
+//     final sql='''SELECT * FROM ${Quotes.quoteTable}''';
+//     final data=await db.rawQuery(sql);
+//     List<Quote> quotesList=List();
 
-    return quotesList;
-  }
+//     for(final node in data){
+//       var temp=Quote.fromJson(node);
+//       quotesList.add(temp);
+//     }
 
-  //function to get quotes of a particular category
-  Future<List<Quote>> getQuoteOfCategory(String category)async{
+//     return quotesList;
+//   }
+
+
+
+//     return quotesList;
+//   }
+
+//   //function to get quotes of an author
+//   Future<List<Quote>> getQuoteOfAuthor(String author)async{
     
-    final sql='''SELECT * FROM ${Quotes.quoteTable} 
-    WHERE ${Quotes.category}==$category
-    ''';
+//     final sql='''SELECT * FROM ${Quotes.quoteTable} 
+//     WHERE ${Quotes.author}==$author
+//     ''';
 
-    final data = await db.rawQuery(sql);
-    List<Quote> quotesList=List();
+//     final data = await db.rawQuery(sql);
+//     List<Quote> quotesList=List();
 
-    for(final node in data){
-      var temp=Quote.fromJson(node);
-      quotesList.add(temp);
-    }
+//     for(final node in data){
+//       var temp=Quote.fromJson(node);
+//       quotesList.add(temp);
+//     }
 
-    return quotesList;
-  }
+//     return quotesList;
+//   }
 
-  //function to get quotes of an author
-  Future<List<Quote>> getQuoteOfAuthor(String author)async{
+//   //function to get quotes of an author
+//   Future<List<dynamic>> getAllCategories()async{
     
-    final sql='''SELECT * FROM ${Quotes.quoteTable} 
-    WHERE ${Quotes.author}==$author
-    ''';
+//     final sql='''SELECT category FROM ${Quotes.quoteTable}''';
 
-    final data = await db.rawQuery(sql);
-    List<Quote> quotesList=List();
+//     final data = await db.rawQuery(sql);
+//     List<dynamic> catList=List();
 
-    for(final node in data){
-      var temp=Quote.fromJson(node);
-      quotesList.add(temp);
-    }
+//     for(final node in data){
+//       var temp=node.toString();
+//       catList.add(temp);
+//     }
 
-    return quotesList;
-  }
+//     return catList;
+//   }
 
-  //update a quotes as a liked quote
-  Future<void> likeQuote(String id)async{
+//   //update a quotes as a liked quote
+//   Future<void> likeQuote(String id)async{
 
-    final sql=''' UPDATE ${Quotes.quoteTable}
-    SET ${Quotes.liked}=1
-    WHERE ${Quotes.id}=$id
-    ''';
-  }  
-}
+//     final sql=''' UPDATE ${Quotes.quoteTable}
+//     SET ${Quotes.liked}=1
+//     WHERE ${Quotes.id}=$id
+//     ''';
+//   }  
