693d5499fdda1dbf35f93bdf0a0882cc1c3c3896
Yash Paneliya <yashsp1158@gmail.com>
2020-07-11 11:59:49 +0530

    Log function removed

--- lib/data/database_creator.dart
+++ lib/data/database_creator.dart
@@ -5,18 +5,4 @@ class Quotes {
   static const author = 'AUTHOR';
   static const liked = 'Liked';
   static const category = 'CATEGORY';
-
-  static void databaseLog(
-      String functionName,
-      String sql,
-      List<Map<String, dynamic>> selectQueryResult,
-      int insertAndUpdateQueryResult) {
-    print(functionName);
-    print(sql);
-    if (selectQueryResult != null) {
-      print(selectQueryResult);
-    } else if (insertAndUpdateQueryResult != null) {
-      print(insertAndUpdateQueryResult);
-    }
-  }
 }
