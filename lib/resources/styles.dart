ec7ff6b7b7d39b1708abade39ea63d1b7fb4fcdb
Yash Paneliya <yashsp1158@gmail.com>
2020-07-12 15:20:09 +0530

    textstyles changed

--- lib/resources/styles.dart
+++ lib/resources/styles.dart
@@ -1,27 +1,26 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
-TextStyle quoteStyle=TextStyle(
-  fontSize: 5.0,
-  color: Colors.white,
-  letterSpacing: 1.0,
-  fontWeight: FontWeight.bold
-);
+TextStyle quoteStyle = TextStyle(
+    fontSize: 5.0,
+    color: Colors.white,
+    letterSpacing: 1.0,
+    fontWeight: FontWeight.bold);
 
-TextStyle authorStyle=TextStyle(
-  fontSize: 25.0,
-  color: Colors.white,
-  letterSpacing: 1.0,
-  fontWeight: FontWeight.bold
-);
+TextStyle authorStyle = TextStyle(
+    fontSize: 25.0,
+    color: Colors.white,
+    letterSpacing: 1.0,
+    fontWeight: FontWeight.bold);
 
-TextStyle menuStyle=TextStyle(
+TextStyle menuStyle =
+    TextStyle(fontWeight: FontWeight.bold, letterSpacing: 1.0);
+
+TextStyle catTitleStyle =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0, letterSpacing: 1.0);
+
+TextStyle authStyle = TextStyle(
   fontWeight: FontWeight.bold,
-  letterSpacing: 1.0
-);
-
-TextStyle catTitleStyle=TextStyle(
-  fontWeight: FontWeight.bold,
-  fontSize: 15.0,
-  letterSpacing: 1.0
+  fontSize: 20.0,
+  letterSpacing: 1.0,
 );
